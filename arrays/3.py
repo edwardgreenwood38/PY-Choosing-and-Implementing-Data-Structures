ad1746d342986541e7aef27793255822aaed228a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-05-14 18:27:40 -0400

    np 3 completed

--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,11 @@
 
+import numpy as np
+
+# Define your array of numbers
+array_of_numbers = np.array([1,6,3])
+
+# Multiply every element by 3
+result_array = array_of_numbers * 3
+
+# Print the result
+print("Result array:", result_array)
