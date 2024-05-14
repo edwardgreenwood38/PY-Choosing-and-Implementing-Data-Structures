79817771facc3eea2ebf85115503fd745b95208b
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-05-14 18:25:00 -0400

    np 1 completed

---
+++ arrays/1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def print_array_items(arr):
+    for item in arr:
+        print(item)
+
+# Example usage:
+numerical_array = [1, 2, 3, 4, 5]
+print_array_items(numerical_array)
