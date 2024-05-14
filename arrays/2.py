6a7b4920f4c2f2af98690a0e42eacf2d407c9c29
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-05-14 18:26:59 -0400

    np 2 completed

---
+++ arrays/2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Define your array of numbers
+array_of_numbers = np.array([1,6,3])
+
+# Calculate the sum
+array_sum = np.sum(array_of_numbers)
+
+# Print the result
+print("Sum of the array:", array_sum)
